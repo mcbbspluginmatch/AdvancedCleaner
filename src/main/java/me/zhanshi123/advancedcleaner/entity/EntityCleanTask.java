7a0aeb7c7a9c1a381c4d2a1e885f9fd14d9151fe
Soldier233 <1224954468@qq.com>
2019-07-23 01:09:20 +0800

    task

--- src/main/java/me/zhanshi123/advancedcleaner/entity/EntityCleanTask.java
+++ src/main/java/me/zhanshi123/advancedcleaner/entity/EntityCleanTask.java
@@ -1,10 +1,21 @@
 package me.zhanshi123.advancedcleaner.entity;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Entity;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.*;
 
 public class EntityCleanTask extends BukkitRunnable {
     @Override
     public void run() {
-
+        Bukkit.getWorlds().forEach(world -> {
+            for (Chunk chunk : world.getLoadedChunks()) {
+                List<Entity> entities = Arrays.asList(chunk.getEntities());
+                Map<Class<? extends Entity>, Set<? extends Entity>> types = new HashMap<>();
+                
+            }
+        });
     }
 }
