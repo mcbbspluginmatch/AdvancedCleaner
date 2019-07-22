6c7aa51d158ab86f19cde9e9511ba00e4578d040
Soldier233 <1224954468@qq.com>
2019-07-23 00:43:02 +0800

    entity clean

---
+++ src/main/java/me/zhanshi123/advancedcleaner/entity/EntityCleanTask.java
@@ -0,0 +1,10 @@
+package me.zhanshi123.advancedcleaner.entity;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class EntityCleanTask extends BukkitRunnable {
+    @Override
+    public void run() {
+
+    }
+}
